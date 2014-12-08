eacfb3e9974088bfe9e820dd60d99f464315557f
Yamei Ou <oym1111@gmail.com>
2014-12-08 09:34:25 -0800

    Change color

--- interactionHand1204.py
+++ interactionHand1204.py
@@ -55,13 +55,16 @@ def init(  ):
     glEnable( GL_LIGHT0 )
     glLightModeli( GL_LIGHT_MODEL_TWO_SIDE, 0 )
     glLightfv( GL_LIGHT0, GL_POSITION, [4, 4, 4, 1] )
-    lA = 1
+    lA = 0.8
     glLightfv( GL_LIGHT0, GL_AMBIENT, [lA, lA, lA, 1] )
     lD = 1
     glLightfv( GL_LIGHT0, GL_DIFFUSE, [lD, lD, lD, 1] )
     lS = 1
     glLightfv( GL_LIGHT0, GL_SPECULAR, [lS, lS, lS, 1] )
-    
+    glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT, [0.0, 0.0, 0.2, 1] )
+    glMaterialfv( GL_FRONT_AND_BACK, GL_DIFFUSE, [0.0, 0.0, 0.7, 1] )
+    glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1] )
+    glMaterialf( GL_FRONT_AND_BACK, GL_SHININESS, 50 )
     global mouseInteractor
     mouseInteractor = MouseInteractor( .01, 1 )
     global tkList
@@ -76,15 +79,7 @@ def init(  ):
 
 def createSphere():
     #glTranslatef(sphere_position[0],sphere_position[1],sphere_position[2])
-    glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT, [0.0, 0.0, 0.2, 1] )
-    glMaterialfv( GL_FRONT_AND_BACK, GL_DIFFUSE, [0.0, 0.0, 0.7, 1] )
-    glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1] )
-    glMaterialf( GL_FRONT_AND_BACK, GL_SHININESS, 50 )
     glutSolidSphere(0.05,32,32)
-    glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT, [0.0, 0.0, 0.2, 1] )
-    glMaterialfv( GL_FRONT_AND_BACK, GL_DIFFUSE, [0.8, 0.8, 0.8, 1] )
-    glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, [0.5, 0.5, 0.5, 1] )
-    glMaterialf( GL_FRONT_AND_BACK, GL_SHININESS, 50 )
    
 
 def drawBone(lengthOfBone,Rangle,Rx,Ry,Rz):
@@ -103,7 +98,6 @@ def connectJoint(lengthOfBone):
     glPopMatrix()
 
 def drawFinger(original_ang, ang_len_tuples):
-
     glPushMatrix()
 
     glRotatef(*original_ang)
